70da35895570de2fbf420d5d581e6ff9e8f1e466
solarconstant <ashishshubh001@gmail.com>
2022-11-24 16:03:58 +0530

    Fix File Drop Size

--- src/components/CodeGeneratorPage.js
+++ src/components/CodeGeneratorPage.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button, Row, Col } from "react-bootstrap";
 import FileDrop from "./FileDrop";
+import CodeBlockComp from "./utils/CodeBlockComp";
 
 import styles from "../assets/css/code_generator_page.css";
+
+const prop_data = {
+  data: {
+    text: "sd",
+    js: "sdf",
+  },
+  language: "json",
+};
 
 const CodeGeneratorPage = () => {
   const [jsonFile, setJsonFile] = useState();
@@ -15,18 +24,30 @@ const CodeGeneratorPage = () => {
   return (
     <div className="code_generator">
       <h2>Code Generator</h2>
-      <Container className="code_generator__container">
-        <FileDrop
-          accepted_file_type="application/json"
-          setCurrentFile={setJsonFile}
-          currentFile={jsonFile}
-        />
-        <FileDrop
-          accepted_file_type="text/csv"
-          setCurrentFile={setCsvFile}
-          currentFile={csvFile}
-        />
+      <Container className="code_generator__container_input">
+        <Row>
+          <Col lg="6">
+            <FileDrop
+              accepted_file_type="application/json"
+              setCurrentFile={setJsonFile}
+              currentFile={jsonFile}
+            />
+          </Col>
+          <Col lg="6">
+            <FileDrop
+              accepted_file_type="text/csv"
+              setCurrentFile={setCsvFile}
+              currentFile={csvFile}
+            />
+          </Col>
+        </Row>
       </Container>
+      <Row className="code_generator__generate-btn">
+        <Col>
+          <Button variant="dark">Generate</Button>
+        </Col>
+      </Row>
+      <hr />
     </div>
   );
 };
